381c4fbc6510246001318909207eb7d58e42f6ae
R.A <roda.ahmed21@gmail.com>
2024-09-24 20:29:19 +0800

    updated django pledge

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -11,4 +11,13 @@ class Project(models.Model):
     is_open = models.BooleanField()
     date_created = models.DateTimeField(auto_now_add=True)
 
+class Pledge(models.Model):
+   amount = models.IntegerField()
+   comment = models.CharField(max_length=200)
+   anonymous = models.BooleanField()
+   project = models.ForeignKey(
+       'Project',
+       on_delete=models.CASCADE,
+       related_name='pledges'
+   )
 
